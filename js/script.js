89ba52820581c895de3237969f0ebb57e57d5810
madsgit0 <landolfomattia@gmail.com>
2022-10-30 20:24:56 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,2 +1,61 @@
 "use strict";
-FontFace;
+class Smartphone {
+    constructor(credito, minutiChiamate, costoChiamate) {
+        this.credito = 0;
+        this.minutiChiamate = 0;
+        this.costoChiamate = 0;
+        this.credito = credito;
+        this.minutiChiamate = minutiChiamate;
+        this.costoChiamate = costoChiamate;
+    }
+    ricarica(soldi) {
+        return this.credito += soldi;
+    }
+    creditoResiduo() {
+        let res = this.credito - this.minutiChiamate * this.costoChiamate;
+    }
+    durataChiamata(minuti) {
+        return this.minutiChiamate += minuti;
+    }
+    mostraDurataChiamate() {
+        return this.minutiChiamate;
+    }
+    azzeraChiamate() {
+        return this.minutiChiamate = 0;
+    }
+}
+class Operatore extends Smartphone {
+    creditoResiduo() {
+        let res = this.credito - this.minutiChiamate * this.costoChiamate;
+    }
+    azzeraCredito() {
+        return this.credito = 0, this.minutiChiamate = 0;
+    }
+    chiamata(minutes) {
+        if (this.credito >= minutes * this.costoChiamate) {
+            this.minutiChiamate += minutes;
+            this.credito -= minutes * this.costoChiamate;
+        }
+    }
+}
+let firstUser = new Operatore(0, 0, 0.2);
+let secondUser = new Operatore(0, 0, 0.2);
+let thirdUser = new Operatore(0, 0, 0.2);
+firstUser.ricarica(15);
+firstUser.chiamata(20);
+firstUser.mostraDurataChiamate();
+firstUser.creditoResiduo();
+secondUser.ricarica(5);
+secondUser.chiamata(30);
+secondUser.mostraDurataChiamate();
+secondUser.creditoResiduo();
+thirdUser.ricarica(52);
+thirdUser.chiamata(8);
+thirdUser.mostraDurataChiamate();
+thirdUser.creditoResiduo();
+console.log(` Il primo utente ha un credito di ${firstUser.credito}€ `);
+console.log(firstUser);
+console.log(` Il secondo utente ha un credito di ${secondUser.credito}€ `);
+console.log(secondUser);
+console.log(` Il terzo utente ha un credito di ${thirdUser.credito}€ `);
+console.log(thirdUser);
--- ts/script.ts
+++ ts/script.ts
@@ -1,1 +1,67 @@
-FontFace
+class Smartphone{
+    credito: number = 0;
+    minutiChiamate: number = 0;
+    costoChiamate: number = 0;
+    constructor (credito: number, minutiChiamate: number, costoChiamate: number){
+        this.credito = credito; 
+        this.minutiChiamate = minutiChiamate;
+        this.costoChiamate = costoChiamate;
+    }
+    
+    ricarica(soldi:number): number{
+        return this.credito += soldi;
+    }
+    creditoResiduo(): any {
+        let res = this.credito - this.minutiChiamate*this.costoChiamate;
+    }
+    durataChiamata(minuti:number):number{
+        return this.minutiChiamate += minuti;
+    }
+    mostraDurataChiamate(): number {
+        return this.minutiChiamate;
+    }
+    azzeraChiamate(): number{
+        return this.minutiChiamate = 0;
+    }
+}
+
+class Operatore extends Smartphone{
+    creditoResiduo(): any {
+        let res = this.credito - this.minutiChiamate * this.costoChiamate;
+    }
+    azzeraCredito(): any{
+        return this.credito = 0, this.minutiChiamate = 0;
+    }
+    chiamata(minutes: number): any {
+        if(this.credito >= minutes * this.costoChiamate){
+            this.minutiChiamate += minutes;
+            this.credito -= minutes * this.costoChiamate;
+        }
+    }
+}
+
+let firstUser = new Operatore (0, 0, 0.2)
+let secondUser = new Operatore (0, 0, 0.2)
+let thirdUser = new Operatore (0, 0, 0.2)
+firstUser.ricarica(15);
+firstUser.chiamata(20);
+firstUser.mostraDurataChiamate();
+firstUser.creditoResiduo();
+
+secondUser.ricarica(5);
+secondUser.chiamata(30);
+secondUser.mostraDurataChiamate();
+secondUser.creditoResiduo();
+
+thirdUser.ricarica(52);
+thirdUser.chiamata(8);
+thirdUser.mostraDurataChiamate();
+thirdUser.creditoResiduo();
+
+console.log(` Il primo utente ha un credito di ${firstUser.credito}€ `)
+console.log(firstUser)
+console.log(` Il secondo utente ha un credito di ${secondUser.credito}€ `)
+console.log(secondUser)
+console.log(` Il terzo utente ha un credito di ${thirdUser.credito}€ `)
+console.log(thirdUser)
+
